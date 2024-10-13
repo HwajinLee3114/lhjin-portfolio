67867894bddeed29cfe33de8ca9da9734c753abb
hjlee3114 <hwajin3114@gmail.com>
2024-10-13 19:57:51 +0900

    이력서 다운로드 구현

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,23 +11,27 @@ export default function About() {
     >
       <div className="h-full p-2 w-full md:w-2/4 flex flex-col items-center justify-center rounded-lg overflow-hidden">
         <ProfileCircle />
-        <div className="pb-5"/>
-        <SlideButton text="이력서 다운로드" href="#" color="#FAAD1A" />
+        <div className="pb-5" />
+        <SlideButton
+          text="이력서 다운로드"
+          url="/files/resume.pdf"
+          isDown={true}
+          color="#FAAD1A"
+        />
       </div>
 
       <div className="h-full flex-grow w-full flex flex-col items-center justify-center">
         <div className="flex gap-5">
-
-        <HoverAMenu 
-          url="https://github.com/HwajinLee3114"
-          title="Git Hub"
-          description="소스 코드 관리"
-        />
-        <HoverAMenu 
-          url="https://lhjini.tistory.com/"
-          title="lhjin.log"
-          description="공부 및 기록 목적의 블로그"
-        />
+          <HoverAMenu
+            url="https://github.com/HwajinLee3114"
+            title="Git Hub"
+            description="소스 코드 관리"
+          />
+          <HoverAMenu
+            url="https://lhjini.tistory.com/"
+            title="lhjin.log"
+            description="공부 및 기록 목적의 블로그"
+          />
         </div>
       </div>
     </section>
--- src/components/button/SlideButton.tsx
+++ src/components/button/SlideButton.tsx
@@ -1,25 +1,54 @@
+"use client";
+
 import React from "react";
 import styles from "./SlideButton.module.css";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 interface SlideButtonProps {
   text: string;
-  href: string;
+  href?: string; // href를 선택적으로 수정
   color: string;
+  isDown?: boolean; // 다운로드 여부
+  url?: string; // 다운로드 URL
 }
 
-const SlideButton: React.FC<SlideButtonProps> = ({ text, href, color }) => {
+const SlideButton: React.FC<SlideButtonProps> = ({
+  text,
+  href,
+  color,
+  isDown,
+  url,
+}) => {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (isDown && url) {
+      event.preventDefault(); // 기본 동작 방지
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = ""; // 다운로드 속성 추가
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else if (href) {
+      // href가 있을 경우에만 페이지 이동
+      window.open(href, "_blank");
+      event.preventDefault(); // 기본 동작 방지
+    }
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <a className={`${styles.link} flex`} href={href} target="_blank">
+    <div className={`${styles.wrapper} cursor-pointer`}>
+      <a
+        className={`${styles.link} flex items-center`}
+        href={href}
+        target="_blank"
+        onClick={handleClick}
+      >
         <div className={styles.color} style={{ backgroundColor: color }}></div>
         <span>{text}</span>
         <FontAwesomeIcon
           icon={faArrowRight}
-          className="w-8 z-10"
-          style={{ marginLeft: "0.5em"}}
+          className="w-8 z-10 ml-2"
         />
       </a>
     </div>
