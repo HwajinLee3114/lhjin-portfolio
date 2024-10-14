15edff9e8c543dd117e9289ab4c0bd9ba79cc0a6
hjlee3114 <hwajin3114@gmail.com>
2024-10-14 22:02:00 +0900

    skills.ts 파일 분리

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,6 +1,6 @@
 import SkillItem from "@/components/skill/SkillItem";
+import { skills } from "@/data/skills";
 import React from "react";
-
 
 export default function Skills() {
   return (
@@ -14,57 +14,14 @@ export default function Skills() {
       </p>
 
       <div className="w-full xl:w-9/12 h-3/5 md:h-96 shadow-md mt-5 rounded-lg bg-themacolor1 p-5 md:p-10">
-        <SkillItem
-          title="FrontEnd"
-          skills={[
-            { name: "JavaScript", color: "#efd81d", txtcolor: "#000000" },
-            { name: "React", color: "#61DBFB" },
-            { name: "TypeScript", color: "#2f74c0" },
-            { name: "Next.js", color: "#000000" },
-            { name: "Zustand", color: "#49443e" },
-            { name: "Tailwind CSS", color: "#06B6D4" },
-          ]}
-          img="/images/fe-100.png"
-        />
-        <SkillItem
-          title="BackEnd"
-          skills={[
-            { name: "Java", color: "#5382a1" },
-            { name: "Spring", color: "#8BC34A" },
-            { name: "Supabase", color: "#30a26e" },
-          ]}
-          img="/images/be-100.png"
-        />
-        <SkillItem
-          title="Database"
-          skills={[
-            { name: "Oracle", color: "#F80102" },
-            { name: "MySQL", color: "#00758f" },
-            { name: "MariaDB", color: "#C49A6C" },
-          ]}
-          img="/images/db-100.png"
-        />
-        <SkillItem
-          title="Tools"
-          skills={[
-            { name: "GitHub", color: "#000000" },
-            // { name: "Git", color: "#F05032" },
-            { name: "PostMan", color: "#EF5B25" },
-            { name: "Slack", color: "#E01E5A" },
-            { name: "Notion", color: "#000000" },
-          ]}
-          img="/images/tool-100.png"
-        />
-        <SkillItem
-          title="Dev Ops"
-          skills={[
-            { name: "Vercel", color: "#000000" },
-            { name: "Netlify", color: "#004746" },
-            // { name: "Docker", color: "#2468ee" },
-            // { name: "AWS", color: "#f79b26", txtcolor:'#000000' },
-          ]}
-          img="/images/dev-100.png"
-        />
+        {skills.map((skill, index) => (
+          <SkillItem
+            key={index}
+            title={skill.title}
+            skills={skill.skills}
+            img={skill.img}
+          />
+        ))}
       </div>
     </section>
   );
