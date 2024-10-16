f881a82fb82b0c961c7dd9d7d96aab6037a9d18f
hjlee3114 <hwajin3114@gmail.com>
2024-10-16 13:38:11 +0900

    career 페이지 작업

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { career } from "@/data/career";
+import { projects } from "@/data/projects";
 
 export default function Career() {
   return (
@@ -7,7 +9,54 @@ export default function Career() {
       className="flex flex-col items-center justify-center min-h-screen bg-themacolor1"
     >
       <h2 className="text-3xl font-bold">Career</h2>
-      <p className="mt-4">내 경력에 대해 알아보세요.</p>
+      <p className="my-4">내 경력에 대해 알아보세요.</p>
+
+      <div className="max-w-2xl w-full">
+        <ul className="flex flex-col gap-5">
+          {career.map((item, index) => (
+            <li key={`carrer_${index}`}>
+              <div className="flex flex-col sm:flex-row gap-2 mb-2 items-start sm:items-center">
+                <h2 className="text-lg font-bold">{item.company}</h2>
+                <div className="text-gray-600 text-base md:text-sm">
+                  {item.period}
+                </div>
+              </div>
+              <div>{item.companyInfo}</div>
+              <div className="flex gap-2">
+                {item.tag.map((t, idx) => (
+                  <p
+                    key={`carrerTag_${idx}`}
+                    className="text-xs px-2 py-0.5 rounded-lg bg-black text-white"
+                  >
+                    {t}
+                  </p>
+                ))}
+              </div>
+
+              {item.projects &&
+                item.projects.map((pjId, idx) => {
+                  const pj = projects.find((p) => p.id === pjId);
+                  return (
+                    pj && (
+                      <section
+                        className="py-3.5 px-0 md:p-4"
+                        key={`${pjId}_carrer_${idx}`}
+                      >
+                        <div className="py-1 pl-4 mb-2 border-l-4 border-gray-600 text-gray-600 bg-quotecolor">
+                          {pj.title}
+                        </div>
+                        <div className="text-gray-600 text-base md:text-sm mb-1">
+                          {pj.period}
+                        </div>
+                        <div>{pj.description}</div>
+                      </section>
+                    )
+                  );
+                })}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
---
+++ src/data/career.ts
@@ -0,0 +1,56 @@
+import { projects } from './projects';
+
+export const career = [
+  /*
+    회사명
+  * 근무 기간(1년 2개월)
+  * 간단 설명
+  * 사용 기슬 스택
+  * 담당 프로젝트명 / 기간 / 한줄 설명 : projects.ts랑 조인해서
+  */
+  {
+    id: '1',
+    company: '회사이름',
+    period: 'yyyy.mm ~ yyyy.mm(n년 n개월)',
+    companyInfo: '',
+    tag: [
+      '111',
+      '2222'
+    ],
+    projects: [
+      '1', '2'
+    ]
+  },
+  {
+    id: '2',
+    company: '회사이름2',
+    period: 'yyyy.mm ~ yyyy.mm(n년 n개월)',
+    companyInfo: '',
+    tag: [
+      '111',
+      '2222'
+    ],
+    projects: [
+      '2'
+    ]
+  },
+  {
+    id: '3',
+    company: '회사이름3',
+    period: 'yyyy.mm ~ yyyy.mm(n년 n개월)',
+    companyInfo: '',
+    tag: [
+      '111',
+      '2222'
+    ],
+    projects: [
+    ]
+  }
+];
+
+const careerWithProjects = career.map(c => ({
+  ...c,
+  projectDetails: c.projects.map(projectId => projects.find(p => p.id === projectId)).filter(Boolean),
+}));
+
+console.info('careerWithProjects:', careerWithProjects);
