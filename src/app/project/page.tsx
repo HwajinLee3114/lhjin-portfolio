ec739a383f988e4991b3902eb223edb3a769f6d7
hjlee3114 <hwajin3114@gmail.com>
2024-10-18 00:44:27 +0900

    update

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -69,7 +69,7 @@ export default function Projects() {
           {filteredPj.map((project, index) => (
             <motion.li
               key={`project_${project.id}`}
-              initial={{ opacity: 0, y: 50 }}
+              initial={{ opacity: 0, y: 30 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{
                 ease: "easeInOut",
