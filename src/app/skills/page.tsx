cb120894719de6264cd07ab5c23551fdee367ba0
hjlee3114 <hwajin3114@gmail.com>
2024-10-18 15:57:45 +0900

    반응형 update

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -8,7 +8,7 @@ export default function Skills() {
       id="skills"
       className="flex flex-col items-center justify-center bg-themacolor2 min-h-50 md:min-h-35 md:h-full"
     >
-      <h2 className="text-3xl g_titleEngFontBlack mt-36 md:mt-0">Skills</h2>
+      <h2 className="text-3xl g_titleEngFontBlack mt-52 md:mt-0">Skills</h2>
       <p className="mt-4">지속적인 변화에 도전하며 개발 역량을 확장하고 있습니다</p>
 
       <div className="w-full xl:w-9/12 h-3/5 md:h-96 shadow-md mt-5 rounded-lg bg-themacolor1 p-5 md:p-10">
